f18a89350ccc16fee711598c0a19b49695ec6fe0
josomm22 <josommefeld@icloud.com>
2019-12-16 14:47:27 +0200

    fixed submit function

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { setStorage, getStorage} from './Storage'
+import { setStorage, getStorage } from './Storage'
 
-class messageObj{
-    constructor(id, login, createdOn, content){
+class messageObj {
+    constructor(id, login, createdOn, content) {
         this.id = id;
         this.login = login;
         this.createdOn = createdOn;
@@ -14,46 +14,62 @@ class Messages extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
-            
+            value: '',
+            messages: [],
+            idCounter: 0
         }
-        this.messages = []
+        // this.messages = []
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    componentDidMount(){
-        this.messages = getStorage()
-        console.log(this.messages);
+    componentDidMount() {
+        const storage = getStorage()
+        console.log(storage);
+        this.setState({ messages: storage })
+
     }
     handleChange(event) {
         this.setState({ value: event.target.value })
     }
     handleSubmit(event) {
-        this.setState({ value: event.target.value })
-        this.messages.push(new messageObj('1','majo','1111',this.state.value))
-        setStorage(this.messages);
+        const { messages } = this.state;
+       // this.setState({ value: event.target.value })
+        this.setState({ messages: [...messages, new messageObj('1', 'majo', '1111', this.state.value)] })
+        setStorage(messages);
 
 
     }
-    Messagebubble(props){
-        return(
-            <p>{props.content}</p>
+    Messagebubble(props) {
+        
+        return (
+            <div>
+                <p>{props.content}</p>
+                <p>
+                    <span>
+                        User: {props.login}
+                    </span>
+                    <span>
+                         Created on: {props.createdOn}
+                    </span>
+                </p>
+            </div>
 
         )
     }
 
     render() {
+        const { messages } = this.state;
         return (
-            <div className = 'mainWrapper'>
-                <div className= 'messageInput'>
-                <textarea rows="4" cols="50" type='text' value={this.state.value} onChange={this.handleChange} />
-                <button type='submit' onClick={this.handleSubmit}> Send</button>
+            <div className='mainWrapper'>
+                <div className='messageInput'>
+                    <textarea rows="4" cols="50" type='text' value={this.state.value} onChange={this.handleChange} />
+                    <button type='submit' onClick={this.handleSubmit}> Send</button>
 
                 </div>
-               
-            <div>
-                {this.messages.map(obj=> <this.Messagebubble content = {obj.content}/>)}
-            </div>
+
+                <div>
+                    {messages.map(obj => <this.Messagebubble key= {this.state.idCounter} login= {obj.login} createdOn= {obj.createdOn} content={obj.content} />)}
+                </div>
             </div>
         )
     }
