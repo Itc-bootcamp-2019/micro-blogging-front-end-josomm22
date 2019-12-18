112d7bb2b745c169efecac143b3952ef17e571dd
josomm22 <josommefeld@icloud.com>
2019-12-18 20:27:27 +0200

    added firebase config to add firestore later

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNbwUom_HFNkM6UdKG6JCwBvXZatjyTHg",
+  authDomain: "micro-blog-josomm22.firebaseapp.com",
+  databaseURL: "https://micro-blog-josomm22.firebaseio.com",
+  projectId: "micro-blog-josomm22",
+  storageBucket: "micro-blog-josomm22.appspot.com",
+  messagingSenderId: "898340409721",
+  appId: "1:898340409721:web:9bdd2721eccbd9f6d15fa8",
+  measurementId: "G-NHZC7FHX0R"
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   
